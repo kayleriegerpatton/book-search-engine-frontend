4416dcfd1e9d45f56b24fca85caaa31083a443a3
Natasha Mann <natasha.s.mann@gmail.com>
2022-03-04 15:59:37 +0000

    removed state variable for user data

--- src/pages/SavedBooks.js
+++ src/pages/SavedBooks.js
@@ -1,5 +1,5 @@
 import { useMutation, useQuery } from "@apollo/client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Jumbotron,
   Container,
@@ -14,60 +14,9 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 export const SavedBooks = () => {
+  let userData;
+
   const [executeRemoveBook] = useMutation(REMOVEBOOK);
-
-  const [userData, setUserData] = useState({});
-
-  // use this to determine if `useEffect()` hook needs to run again
-  // const userDataLength = Object.keys(userData).length;
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
-
-  // get user data
-
-  const { loading, error, data } = useQuery(GETME);
-
-  useEffect(() => {
-    try {
-      if (data) {
-        setUserData(data.me);
-        // console.log("data.me:", data.me);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }, [userData]);
-
-  if (error) {
-    throw new Error("something went wrong!");
-  }
-
-  if (loading) {
-    return <h2>LOADING...</h2>;
-  }
 
   // accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -78,19 +27,32 @@ export const SavedBooks = () => {
     }
 
     try {
-      const { data } = await executeRemoveBook({
+      await executeRemoveBook({
         variables: {
           bookId,
         },
       });
 
-      setUserData(data);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
+
+  const { loading, error, data } = useQuery(GETME);
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  if (error) {
+    throw new Error("something went wrong!");
+  }
+
+  if (data) {
+    userData = data.me;
+  }
 
   return (
     <>
@@ -106,14 +68,14 @@ export const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {data.me.savedBooks.length
-            ? `Viewing ${data.me.savedBooks.length} saved ${
-                data.me.savedBooks.length === 1 ? "book" : "books"
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
               }:`
             : "You have no saved books!"}
         </h2>
         <CardColumns>
-          {data.me.savedBooks.map((book) => {
+          {userData.savedBooks.map((book) => {
             return (
               <Card key={book.bookId} border="dark">
                 {book.image ? (
